90e52537321d0a69b27a264425a60ccf822f8579
Benjamin Albrecht <benjamin.albrecht@dbschenker.com>
2017-12-29 23:43:43 +0100

    Add logger4j

--- 02_Compiler/src/main/java/vong/piler/her/parser/Parser.java
+++ 02_Compiler/src/main/java/vong/piler/her/parser/Parser.java
@@ -2,13 +2,31 @@ package vong.piler.her.parser;
 
 import java.util.List;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+import vong.piler.her.Constants;
 import vong.piler.her.lexer.Token;
 
-public class Parser {    
+public class Parser { 
+	
+	private static Logger logger = LogManager.getLogger(Constants.loggerName);
+	
     public Parser() {
     }
     
     public void parse(List<Token> tokenList) {
+    	
+		System.out.println("Parser:");
+    	
+    	for (Token t : tokenList) {
+
+    		System.out.println(t);
+    	}
+    	
+    	
+    	
+    	
     }
     
     private void parseItem() {
