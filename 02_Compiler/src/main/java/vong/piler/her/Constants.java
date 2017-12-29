8402bbf335cfff063b69ddb7017409e19b2a7488
Fabio Spiekermann <fspiekermann@itemis.de>
2017-12-29 03:19:54 +0100

    Implement first logger and add constant for logger name

---
+++ 02_Compiler/src/main/java/vong/piler/her/Constants.java
@@ -0,0 +1,11 @@
+package vong.piler.her;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+public class Constants {
+	
+	// Bsp. for logger private static Logger logger = LogManager.getLogger(Constants.loggerName);
+	public static final String loggerName = "VongPiler";
+
+}
--- 02_Compiler/src/main/java/vong/piler/her/generator/Generator.java
+++ 02_Compiler/src/main/java/vong/piler/her/generator/Generator.java
@@ -1,6 +1,15 @@
 package vong.piler.her.generator;
 
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+import vong.piler.her.Constants;
+
 public class Generator {
+	
+	private static Logger logger = LogManager.getLogger(Constants.loggerName);
+	
     public Generator() {
         
     }
