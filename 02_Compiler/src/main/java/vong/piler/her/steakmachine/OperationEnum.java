b479dca3c4cfd17ac76ffa9b48b0097bef0d6476
SLE <SLE@NLW-2323.opitz-consulting.int>
2017-12-29 12:49:03 +0100

    First implementation of steackmachine with support for PSZ, ADD, PRT and END

--- 02_Compiler/src/main/java/vong/piler/her/steakmachine/OperationEnum.java
+++ 02_Compiler/src/main/java/vong/piler/her/steakmachine/OperationEnum.java
@@ -6,24 +6,34 @@ package vong.piler.her.steakmachine;
  */
 
 public enum OperationEnum {
-    PSA, // push address to stack
-    PSZ, // push number(zal) to stack
-    PSI, // push boolean(isso) to stack
-    ADD, // arg1 + arg2
-    SUB, // arg1 - arg2
-    MUL, // arg1 * arg2
-    DIV, // arg1 / arg2
-    MOD, // arg1 % arg2
-    LES, // arg1 < arg2
-    EQL, // arg1 == arg2
-    NQL, // arg1 != arg2
-    GTR, // arg1 > arg2
-    AND, // arg1 && arg2
-    OHR, // arg1 || arg2
-    JMP, // jump to address(arg)
-    JMT, // jump to address(arg) if previous compare was true
-    SAV, // save arg in data-memory
-    PRT, // prints arg
-    AAL, // prints "hello world!"
-    END  // end of code
+    PSA(1), // push address to stack
+    PSZ(1), // push number(zal) to stack
+    PSI(1), // push boolean(isso) to stack
+    ADD(0), // arg1 + arg2
+    SUB(0), // arg1 - arg2
+    MUL(0), // arg1 * arg2
+    DIV(0), // arg1 / arg2
+    MOD(0), // arg1 % arg2
+    LES(0), // arg1 < arg2
+    EQL(0), // arg1 == arg2
+    NQL(0), // arg1 != arg2
+    GTR(0), // arg1 > arg2
+    AND(0), // arg1 && arg2
+    OHR(0), // arg1 || arg2
+    JMP(0), // jump to address(arg)
+    JMT(0), // jump to address(arg) if previous compare was true
+    SAV(0), // save arg in data-memory
+    PRT(0), // prints arg
+    AAL(0), // prints "hello world!"
+    END(0);  // end of code
+	
+	private int argCount;
+	
+	OperationEnum(int argCount) {
+		this.argCount = argCount;
+	}
+	
+	public int getArgCount() {
+		return argCount;
+	}
 }
