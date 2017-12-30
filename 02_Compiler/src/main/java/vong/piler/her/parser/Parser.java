0301462ef55ba55be68345a85c291e5852123af6
Benjamin Albrecht <benjamin.albrecht@dbschenker.com>
2017-12-30 14:37:06 +0100

    First version parser

--- 02_Compiler/src/main/java/vong/piler/her/parser/Parser.java
+++ 02_Compiler/src/main/java/vong/piler/her/parser/Parser.java
@@ -1,5 +1,6 @@
 package vong.piler.her.parser;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.EnumMap;
 import java.util.List;
@@ -16,51 +17,84 @@ public class Parser {
 	
 	private static Logger logger = LogManager.getLogger(Constants.loggerName);
 	
+	private TreeNode parent;
+	private TreeNode root;
+	
 	Map<TokenTypeEnum, List<TokenTypeEnum>> ruleMap = new EnumMap<TokenTypeEnum, List<TokenTypeEnum>>(TokenTypeEnum.class);
 	
-	   
-	
-    public Parser() {    
-    ruleMap.put(TokenTypeEnum.START,Arrays.asList(new TokenTypeEnum [] {TokenTypeEnum.VSTART}));
-    ruleMap.put(TokenTypeEnum.VSTART,Arrays.asList(new TokenTypeEnum [] {TokenTypeEnum.TYPE}));
-    ruleMap.put(TokenTypeEnum.TYPE,Arrays.asList(new TokenTypeEnum [] {TokenTypeEnum.NAME}));
-    ruleMap.put(TokenTypeEnum.NAME,Arrays.asList(new TokenTypeEnum [] {TokenTypeEnum.ASSI, TokenTypeEnum.PEND, TokenTypeEnum.PNEXT, TokenTypeEnum.PSTART}));
-    ruleMap.put(TokenTypeEnum.ASSI,Arrays.asList(new TokenTypeEnum [] {TokenTypeEnum.CONST_ISSO, TokenTypeEnum.CONST_WORD, TokenTypeEnum.CONST_ZAL}));
-    ruleMap.put(TokenTypeEnum.CONST_ISSO,Arrays.asList(new TokenTypeEnum [] {TokenTypeEnum.PEND, TokenTypeEnum.PNEXT, TokenTypeEnum.VEND}));
-    ruleMap.put(TokenTypeEnum.CONST_WORD,Arrays.asList(new TokenTypeEnum [] {TokenTypeEnum.PEND, TokenTypeEnum.PNEXT, TokenTypeEnum.VEND}));
-    ruleMap.put(TokenTypeEnum.CONST_ZAL,Arrays.asList(new TokenTypeEnum [] {TokenTypeEnum.PEND, TokenTypeEnum.PNEXT, TokenTypeEnum.VEND}));
-    ruleMap.put(TokenTypeEnum.VEND,Arrays.asList(new TokenTypeEnum [] {TokenTypeEnum.VSTART, TokenTypeEnum.CMD}));
-    ruleMap.put(TokenTypeEnum.CMD,Arrays.asList(new TokenTypeEnum [] {TokenTypeEnum.NAME}));
-    ruleMap.put(TokenTypeEnum.PSTART,Arrays.asList(new TokenTypeEnum [] {TokenTypeEnum.CONST_ISSO, TokenTypeEnum.CONST_WORD, TokenTypeEnum.CONST_ZAL, TokenTypeEnum.PEND, TokenTypeEnum.NAME}));
-    ruleMap.put(TokenTypeEnum.PNEXT,Arrays.asList(new TokenTypeEnum [] {TokenTypeEnum.CONST_ISSO, TokenTypeEnum.CONST_WORD, TokenTypeEnum.CONST_ZAL}));
-    ruleMap.put(TokenTypeEnum.PEND,Arrays.asList(new TokenTypeEnum [] {TokenTypeEnum.CMD, TokenTypeEnum.END}));
-    ruleMap.put(TokenTypeEnum.END,Arrays.asList(new TokenTypeEnum [] {}));
+    public Parser() {
+    	// Add rules to map
+	    ruleMap.put(TokenTypeEnum.START,Arrays.asList(new TokenTypeEnum [] {TokenTypeEnum.VSTART}));
+	    ruleMap.put(TokenTypeEnum.VSTART,Arrays.asList(new TokenTypeEnum [] {TokenTypeEnum.TYPE}));
+	    ruleMap.put(TokenTypeEnum.TYPE,Arrays.asList(new TokenTypeEnum [] {TokenTypeEnum.NAME}));
+	    ruleMap.put(TokenTypeEnum.NAME,Arrays.asList(new TokenTypeEnum [] {TokenTypeEnum.ASSI, TokenTypeEnum.PEND, TokenTypeEnum.PNEXT, TokenTypeEnum.PSTART}));
+	    ruleMap.put(TokenTypeEnum.ASSI,Arrays.asList(new TokenTypeEnum [] {TokenTypeEnum.CONST_ISSO, TokenTypeEnum.CONST_WORD, TokenTypeEnum.CONST_ZAL}));
+	    ruleMap.put(TokenTypeEnum.CONST_ISSO,Arrays.asList(new TokenTypeEnum [] {TokenTypeEnum.PEND, TokenTypeEnum.PNEXT, TokenTypeEnum.VEND}));
+	    ruleMap.put(TokenTypeEnum.CONST_WORD,Arrays.asList(new TokenTypeEnum [] {TokenTypeEnum.PEND, TokenTypeEnum.PNEXT, TokenTypeEnum.VEND}));
+	    ruleMap.put(TokenTypeEnum.CONST_ZAL,Arrays.asList(new TokenTypeEnum [] {TokenTypeEnum.PEND, TokenTypeEnum.PNEXT, TokenTypeEnum.VEND}));
+	    ruleMap.put(TokenTypeEnum.VEND,Arrays.asList(new TokenTypeEnum [] {TokenTypeEnum.VSTART, TokenTypeEnum.CMD}));
+	    ruleMap.put(TokenTypeEnum.CMD,Arrays.asList(new TokenTypeEnum [] {TokenTypeEnum.NAME}));
+	    ruleMap.put(TokenTypeEnum.PSTART,Arrays.asList(new TokenTypeEnum [] {TokenTypeEnum.CONST_ISSO, TokenTypeEnum.CONST_WORD, TokenTypeEnum.CONST_ZAL, TokenTypeEnum.PEND, TokenTypeEnum.NAME}));
+	    ruleMap.put(TokenTypeEnum.PNEXT,Arrays.asList(new TokenTypeEnum [] {TokenTypeEnum.CONST_ISSO, TokenTypeEnum.CONST_WORD, TokenTypeEnum.CONST_ZAL}));
+	    ruleMap.put(TokenTypeEnum.PEND,Arrays.asList(new TokenTypeEnum [] {TokenTypeEnum.CMD, TokenTypeEnum.END}));
+	    ruleMap.put(TokenTypeEnum.END,Arrays.asList(new TokenTypeEnum [] {}));
     }
     
-    public void parse(List<Token> tokenList) {
+    public TreeNode parse(List<Token> tokenList) {
     	
+    	List<TokenTypeEnum> rule = new ArrayList<TokenTypeEnum>();    	
 
     	
     	for (Token t : tokenList) {
     		
-    		if (!(t.equals(TokenTypeEnum.START))) {
-    		
+    		// Toke != START
+    		if (!(t.getType().equals(TokenTypeEnum.START))) {    			
+    			// Syntax ok
+    			if(rule.contains(t.getType())){    				
+    				parseItem(t);		
+    				
+    			}
+    			//Syntax fail
+    			else {    				
+    				System.out.println("Syntax error:" + t);
+    				System.exit(0);
+    			}
+    		// Token == START and first token
+    		}else if(t.getType().equals(TokenTypeEnum.START) && rule.isEmpty()){
+    			System.out.println("Token:" + t.getType());
+    			parseItem(t);
     			
+    		}else {
+				System.out.println("Syntax error: START missing" + t);
+				System.exit(0);
     		}
     		
-   
-    		
-    		
-    		
-    		
+			rule = ruleMap.get(t.getType());    		
     	}
     	
-    	
-    	
-    	
+    	return root;
+    	   	
     }
     
-    private void parseItem() {
-        
+    private void parseItem(Token t) {
+    	
+    	// Root node
+    	if (root == null) {
+          root = new TreeNode(t.getType());
+          parent = root;
+    	}
+    	// Node without value
+    	else if(t.getValue().isEmpty()) {
+			System.out.println("Token: " + t.getType());
+	        parent.setRight(new TreeNode(t.getType()));
+	        parent = parent.getRight();
+
+	    // Node with value
+		}else {
+			System.out.println("Value: " + t.getValue() + " Token: " + t.getType());
+			parent.setRight(new TreeNode(t.getType()));
+			parent = parent.getRight();
+           	parent.setLeft(t.getValue());
+		}       
     }
 }
