7e961d0916ce3b0321e78937e0668b92062f6c0b
Fabio Spiekermann <fspiekermann@itemis.de>
2017-12-30 13:12:54 +0100

    fixed logger

--- 02_Compiler/src/main/java/vong/piler/her/Constants.java
+++ 02_Compiler/src/main/java/vong/piler/her/Constants.java
@@ -2,7 +2,6 @@ package vong.piler.her;
 
 public class Constants {
 	
-	// Bsp. for logger private static Logger logger = LogManager.getLogger(Constants.loggerName);
 	public static final String loggerName = "VongPiler";
 
 }
