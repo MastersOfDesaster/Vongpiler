eb9466fe51931e07b0d16138c4f2975665170375
Fabio Spiekermann <fspiekermann@itemis.de>
2017-12-31 14:04:12 +0100

    Edit operation

--- 04_runtime/vongvirtualmachine/src/main/java/vong/piler/her/vongruntime/virtualmachine/model/OperationEnum.java
+++ 04_runtime/vongvirtualmachine/src/main/java/vong/piler/her/vongruntime/virtualmachine/model/OperationEnum.java
@@ -6,34 +6,27 @@ package vong.piler.her.vongruntime.virtualmachine.model;
  */
 
 public enum OperationEnum {
-/*00*/    PSA(1), // push address to stack
-/*01*/    PSZ(1), // push number(zal) to stack
-/*02*/    PSI(1), // push boolean(isso) to stack
-/*03*/    PSW(1),
-/*04*/    ADD(0), // arg1 + arg2
-/*05*/    SUB(0), // arg1 - arg2
-/*06*/    MUL(0), // arg1 * arg2
-/*07*/    DIV(0), // arg1 / arg2
-/*08*/    MOD(0), // arg1 % arg2
-/*09*/    LES(0), // arg1 < arg2
-/*10*/    EQZ(0), // arg1 == arg2
-/*11*/    EQI(0),
-/*12*/    NQZ(0), // arg1 != arg2
-/*13*/    NQI(0),
-/*14*/    GTR(0), // arg1 > arg2
-/*15*/    AND(0), // arg1 && arg2
-/*16*/    OHR(0), // arg1 || arg2
-/*17*/    JMP(0), // jump to address(arg)
-/*18*/    JMT(0), // jump to address(arg) if previous compare was true
-/*19*/    PRT(0), // prints arg
-/*20*/    AAL(0), // prints "hello world!"
-/*21*/    SVA(0),
-/*22*/    SVZ(0),
-/*23*/    SVI(0),
-/*24*/    SVW(0),
-/*25*/    EQW(0),
-/*26*/    NQW(0),
-/*27*/    END(0);  // end of code
+	/*00*/    PSA(1), // push address to stack
+	/*01*/    PSZ(1), // push number(zal) to stack
+	/*02*/    PSI(1), // push boolean(isso) to stack
+	/*03*/    PSW(1),
+	/*04*/    ADD(0), // arg1 + arg2
+	/*05*/    SUB(0), // arg1 - arg2
+	/*06*/    MUL(0), // arg1 * arg2
+	/*07*/    DIV(0), // arg1 / arg2
+	/*08*/    MOD(0), // arg1 % arg2
+	/*09*/    LES(0), // arg1 < arg2
+	/*10*/    EQL(0), // arg1 == arg2
+	/*11*/    NOT(0), // arg1 != arg2
+	/*12*/    GTR(0), // arg1 > arg2
+	/*13*/    AND(0), // arg1 && arg2
+	/*14*/    OHR(0), // arg1 || arg2
+	/*15*/    JMP(0), // jump to address(arg)
+	/*16*/    JMT(0), // jump to address(arg) if previous compare was true
+	/*17*/    PRT(0), // prints arg
+	/*18*/    AAL(0), // prints "hello world!"
+	/*19*/    SAV(0),
+	/*20*/    END(0);  // end of code
 	
 	private int argCount;
 	
