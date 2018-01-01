0b52cedd3f9bb157a49719945441e2629d95edaa
Sebastian Lechte <sebastian.lechte@hs-bochum.de>
2018-01-01 18:32:05 +0100

    Reworked maven configuration
    Added maven assembly plugin
    Added apache commons cli dependency

--- 02_Compiler/src/main/java/vong/piler/her/Main.java
+++ 02_Compiler/src/main/java/vong/piler/her/Main.java
@@ -6,8 +6,6 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 
-import org.junit.runners.parameterized.TestWithParameters;
-
 import vong.piler.her.generator.Generator;
 import vong.piler.her.lexer.Lexer;
 import vong.piler.her.lexer.Token;
